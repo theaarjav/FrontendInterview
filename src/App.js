929db00a5c7e145e012b83e83c2fa52e72dd0670
theaarjav <aarjavdesai22@gmail.com>
2024-07-24 12:22:00 +0530

    added Frontend task to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+/*
+  Create a list of candidates, showing their name, vote count, and buttons to add/subtract votes
+  Create a small form which has an input box for candidate names and a SUBMIT button to add candidate names. 
+  Writing a name in this box and clicking "SUBMIT" should add this candidate name in the list with vote count: 0
+  Show the list in the order of descending number of votes
+*/
 function App() {
+  const [list, setList] = useState([])
+  const [newName, setNewName] = useState("")
+  const handleVoteCount=(ind, fun)=>{
+    var newList=list;
+    console.log(newList[ind]);
+   if(fun=='-') newList[ind].votes--;
+   else newList[ind].votes++;
+   setList(newList)
+  }
+
+  const handleChange=(e)=>{
+    setNewName(e.target.value)
+  }
+
+
+
+  const handleSubmit=()=>{
+    setList([...list, {name: newName, votes:0}]);
+    setNewName("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {
+        list.map(({name, votes, }, i)=>{
+          return <div className='flex justify-between items-center'>
+            <button onClick={()=>handleVoteCount(i, '-')}>-</button>
+            <span>
+              {name}
+            </span>
+            <span>{votes}</span>
+            <button onClick={()=>handleVoteCount(i, '+')}>+</button>
+          </div>
+        })
+      }
+
+      <div>
+        Add Name:
+        <input placeholder='Add name to election' value={newName} type='text' onChange={handleChange}/>
+        <button onClick={handleSubmit}>Add {newName}</button>
+      </div>
     </div>
   );
 }
